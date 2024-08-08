868aef6f2ac1a29578670b4ebde28596b62cff81
kelvinnede <kelvinnede@gmail.com>
2024-08-08 01:16:34 -0700

    task4

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+-- Drop the trigger if it exists
+DROP TRIGGER IF EXISTS decrease_quantity;
+
+-- Create the trigger
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
